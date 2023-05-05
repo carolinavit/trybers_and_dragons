8e7725b6d73b807ac2de8b0cce1d7e2b568432ac
Vitória <vitcs11@gmail.com>
2023-05-05 16:54:21 -0300

    feat(src/Races): Cria classe Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,19 @@
+abstract class Race {
+  constructor(private _name: string, private _dexterity: number) {}
+
+  public get name() {
+    return this._name;
+  }
+
+  public get dexterity() {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
